36c125c0f1a6b6ac7323877f8980d49c5c0ae491
Nathan Haliday <149752537+NathanHaliday@users.noreply.github.com>
2025-03-10 12:44:18 +1300

    Update not-found.tsx

--- client/src/pages/not-found.tsx
+++ client/src/pages/not-found.tsx
@@ -12,7 +12,7 @@ export default function NotFound() {
           </div>
 
           <p className="mt-4 text-sm text-gray-600">
-            Did you forget to add the page to the router?
+            Message from Nathan: Something isn't working, You need to fix it. Something something router?
           </p>
         </CardContent>
       </Card>
